d76e32257e81476c233251cf5442d257d89a4c99
Norring <anorring2@gmail.com>
2026-08-17 23:58:53 +0200

    chore: bump version to 1.0.1

--- build.ps1
+++ build.ps1
@@ -3,7 +3,3 @@ $env:TAURI_SIGNING_PRIVATE_KEY = $key
 $env:TAURI_SIGNING_PRIVATE_KEY_PASSWORD = 'crosshair-update.key'
 Set-Location 'C:\roblox-crosshair-manager'
 & cargo tauri build
-Write-Host ""
-Write-Host "Build complete! Sig files are in:"
-Write-Host "  NSIS: src-tauri\target\release\bundle\nsis\"
-Write-Host "  MSI:  src-tauri\target\release\bundle\msi\"
